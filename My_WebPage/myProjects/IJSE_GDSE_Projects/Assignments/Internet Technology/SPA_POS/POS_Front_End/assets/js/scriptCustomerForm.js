c2d67e05b8e2c269a98158d1f6c6d64df5a5ef27
Kavindu Deshan <kkavindu.deshan@gmail.com>
2023-02-07 09:37:22 +0530

    Added Validations For the Customer Form - SPA_POS

--- My_WebPage/myProjects/IJSE_GDSE_Projects/Assignments/Internet Technology/SPA_POS/POS_Front_End/assets/js/scriptCustomerForm.js
+++ My_WebPage/myProjects/IJSE_GDSE_Projects/Assignments/Internet Technology/SPA_POS/POS_Front_End/assets/js/scriptCustomerForm.js
@@ -1,25 +1,146 @@
+$("#txtCusID").focus();
+
+/* CUSTOMER TEXT FIELDS VALIDATION */
+const cusIDRegEx = /^(C00-)[0-9]{1,3}$/;
+const cusNameRegEx = /^[A-z ]{5,20}$/;
+const cusAddressRegEx = /^[0-9/A-z. ,]{7,}$/;
+const cusPhnNumRegEx =
+  /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+
+let customerValidations = [];
+customerValidations.push({
+  reg: cusIDRegEx,
+  field: $("#txtCusID"),
+  error: "Customer ID Pattern is : C00-001",
+});
+customerValidations.push({
+  reg: cusNameRegEx,
+  field: $("#txtCusName"),
+  error: "Customer Name Pattern is : A-z 5-20",
+});
+customerValidations.push({
+  reg: cusAddressRegEx,
+  field: $("#txtCusAddress"),
+  error: "Customer Address Pattern is : A-z 0-9 ,/",
+});
+customerValidations.push({
+  reg: cusPhnNumRegEx,
+  field: $("#txtCusPhnNum"),
+  error: "Customer Phone Number Is Wrong. Please Check..",
+});
+
+$("#txtCusID, #txtCusName, #txtCusAddress, #txtCusPhnNum").on(
+  "keyup",
+  function () {
+    checkValidity();
+  }
+);
+
+$("#txtCusID, #txtCusName, #txtCusAddress, #txtCusPhnNum").on(
+  "blur",
+  function () {
+    checkValidity();
+  }
+);
+
+function checkValidity() {
+  let errorCount = 0;
+
+  for (let validation of customerValidations) {
+    if (check(validation.reg, validation.field)) {
+      setTextSuccess(validation.field, "");
+    } else {
+      errorCount = errorCount + 1;
+      setTextError(validation.field, validation.error);
+    }
+  }
+
+  setButtonState(errorCount);
+}
+
+function setButtonState(errorCount) {
+  if (errorCount > 0) {
+    $("#btnSaveCustomer").attr("disabled", true);
+  } else {
+    $("#btnSaveCustomer").attr("disabled", false);
+  }
+}
+
+function setTextError(txtField, error) {
+  if (txtField.val().length <= 0) {
+    defaultText(txtField, "");
+  } else {
+    txtField.css("border", "2px solid red");
+    txtField.parent().children("span").text(error);
+    txtField.parent().children("span").css("color", "red");
+  }
+}
+
+function setTextSuccess(txtField, error) {
+  if (txtField.val().length <= 0) {
+    defaultText(txtField, "");
+  } else {
+    txtField.css("border", "2px solid green");
+    txtField.parent().children("span").text(error);
+  }
+}
+
+function defaultText(txtField, error) {
+  txtField.css("border", "1px solid #ced4da");
+  txtField.parent().children("span").text(error);
+}
+/* DISABLE "TAB" KEY DEFAULT FUNCTION */
+$("#txtCusID, #txtCusName, #txtCusAddress, #txtCusPhnNum").keydown(function (
+  event
+) {
+  if (event.key == "Tab") {
+    event.preventDefault();
+  }
+});
+
+function check(regEx, txtFieldId) {
+  let inputValue = txtFieldId.val();
+
+  if (regEx.test(inputValue)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/* /CUSTOMER TEXT FIELDS VALIDATION */
+
 $("#txtCusID").keydown(function (event) {
-  if (event.keyCode == 13) {
+  if (event.keyCode == 13 && check(cusIDRegEx, $("#txtCusID"))) {
     $("#txtCusName").focus();
+  } else {
+    $("#txtCusID").focus();
   }
 });
 
 $("#txtCusName").keydown(function (event) {
-  if (event.keyCode == 13) {
+  if (event.keyCode == 13 && check(cusNameRegEx, $("#txtCusName"))) {
     $("#txtCusAddress").focus();
   }
 });
 
 $("#txtCusAddress").keydown(function (event) {
-  if (event.keyCode == 13) {
+  if (event.keyCode == 13 && check(cusAddressRegEx, $("#txtCusAddress"))) {
     $("#txtCusPhnNum").focus();
   }
 });
 
 $("#txtCusPhnNum").keydown(function (event) {
-  if (event.keyCode == 13) {
-    saveCustomer();
-    $("#txtCusID").focus();
+  if (
+    event.keyCode == 13 &&
+    check(cusPhnNumRegEx, $("#txtCusPhnNum")) &&
+    !($("#btnSaveCustomer").attr("disabled") == "disabled")
+  ) {
+    let res = confirm("Do you want to add this customer ?");
+    if (res) {
+      saveCustomer();
+      $("#txtCusID").focus();
+    }
   }
 });
 
