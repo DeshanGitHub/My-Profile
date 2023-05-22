2459245d90935c571de8d8d2d114f00352f82f55
Kavindu Deshan <kkavindu.deshan@gmail.com>
2023-05-22 22:15:17 +0530

    Removed Unwanted Package

--- My_WebPage/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-/*SMALL NAV DROP-DOWN*/
-$(document).click(function () {
-    if ($(event.target).closest(".menu-btn").length != 0)return false;
-    $(".small-drop-down").fadeOut(400);
-});
-$(".small-drop-down").hide(0);
-$(".menu-btn").click(function () {
-    $(".small-drop-down").fadeToggle(400);
-});
