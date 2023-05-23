0e48a3f46c5c8ef30285967feab088a334eaca97
Kavindu Deshan <kkavindu.deshan@gmail.com>
2023-05-23 12:44:41 +0530

    Added My Projects Area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
+/*OWL CAROUSEL*/
+$('.my-work').owlCarousel({
+    animateOut: 'slideOutDown',
+    autoplay:'true',
+    autoplayTimeout: '3000',
+    autoplaySpeed: '1000',
+    navText: ['<span class="jam jam-arrow-left"></span>','<span class="jam jam-arrow-right"></span>'],
+    loop:true,
+    margin:24,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        568:{
+            items:2
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
 /*SMALL NAV DROP-DOWN*/
 $(document).click(function () {
     if ($(event.target).closest(".menu-btn").length != 0)return false;
