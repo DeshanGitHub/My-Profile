c5360f9d40df2b69deb5c4628dc3976d46f05565
Kavindu Deshan <kkavindu.deshan@gmail.com>
2023-05-31 06:14:29 +0530

    Added Home Page

--- myProjects/IJSE_GDSE_Projects/Assignments/Internet Technology/SPA_POS/POS_Front_End/assets/js/script.js
+++ myProjects/IJSE_GDSE_Projects/Assignments/Internet Technology/SPA_POS/POS_Front_End/assets/js/script.js
@@ -1,11 +1,39 @@
 $("#itemSectionId").css("display", "none");
-$("#customerSectionId").css("display", "block");
+$("#customerSectionId").css("display", "none");
 $("#orderSectionId").css("display", "none");
+$("#dashboardSectionId").css("display", "block");
+
+/*ADD CURSOR POINTER TO HEADER BUTTONS*/
+$("#customerFormLoadButtonId").css("cursor","pointer");
+$("#dashboardFormLoadButtonId").css("cursor","pointer");
+$("#itemFormLoadButtonId").css("cursor","pointer");
+$("#orderFormLoadButtonId").css("cursor","pointer");
+
+$("#dashboardFormLoadButtonId").click(function () {
+  $("#itemSectionId").css("display", "none");
+  $("#customerSectionId").css("display", "none");
+  $("#orderSectionId").css("display", "none");
+  $("#dashboardSectionId").css("display", "block");
+
+  /*MANAGE BUTTON CSS ACTIVE*/
+  $("#customerFormLoadButtonId").removeClass("active");
+  $("#dashboardFormLoadButtonId").addClass("active");
+  $("#itemFormLoadButtonId").removeClass("active");
+  $("#orderFormLoadButtonId").removeClass("active");
+
+});
 
 $("#itemFormLoadButtonId").click(function () {
   $("#itemSectionId").css("display", "block");
   $("#customerSectionId").css("display", "none");
   $("#orderSectionId").css("display", "none");
+  $("#dashboardSectionId").css("display", "none");
+
+  /*MANAGE BUTTON CSS ACTIVE*/
+  $("#customerFormLoadButtonId").removeClass("active");
+  $("#dashboardFormLoadButtonId").removeClass("active");
+  $("#itemFormLoadButtonId").addClass("active");
+  $("#orderFormLoadButtonId").removeClass("active");
 
   /* ITEM TEXT FIELD FOCUS WHEN OPEN ITEM FIELDS */
   $("#txtItemCode").focus();
@@ -18,6 +46,14 @@ $("#customerFormLoadButtonId").click(function () {
   $("#itemSectionId").css("display", "none");
   $("#customerSectionId").css("display", "block");
   $("#orderSectionId").css("display", "none");
+  $("#dashboardSectionId").css("display", "none");
+
+  /*MANAGE BUTTON CSS ACTIVE*/
+  $("#customerFormLoadButtonId").addClass("active");
+  $("#dashboardFormLoadButtonId").removeClass("active");
+  $("#itemFormLoadButtonId").removeClass("active");
+  $("#orderFormLoadButtonId").removeClass("active");
+
 
   /* CUSTOMER TEXT FIELD FOCUS WHEN OPEN CUSTOMER FIELDS */
   $("#txtCusID").focus();
@@ -27,4 +63,11 @@ $("#orderFormLoadButtonId").click(function () {
   $("#itemSectionId").css("display", "none");
   $("#customerSectionId").css("display", "none");
   $("#orderSectionId").css("display", "block");
+  $("#dashboardSectionId").css("display", "none");
+
+  /*MANAGE BUTTON CSS ACTIVE*/
+  $("#customerFormLoadButtonId").removeClass("active");
+  $("#dashboardFormLoadButtonId").removeClass("active");
+  $("#itemFormLoadButtonId").removeClass("active");
+  $("#orderFormLoadButtonId").addClass("active");
 });
